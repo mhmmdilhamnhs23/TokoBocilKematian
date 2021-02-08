3ee7c6668c68edda29a893d4bb5f8ff140c5a4dc
JasmineAR985 <muhammadilhamnurhasani@gmail.com>
2021-02-08 13:01:39 +0700

    v4

---
+++ app/src/main/java/com/example/tokobocilkematian/AkunActivity.java
@@ -0,0 +1,14 @@
+package com.example.tokobocilkematian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AkunActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_akun);
+    }
+}
--- app/src/main/java/com/example/tokobocilkematian/MainActivity.java
+++ app/src/main/java/com/example/tokobocilkematian/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tokobocilkematian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void chat(View view) {
+        Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+        startActivity(intent);
+    }
+    public void home(View view) {
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
+    }
+    public void setting(View view) {
+        Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+        startActivity(intent);
+    }
+    public void akun(View view) {
+        Intent intent = new Intent(MainActivity.this, AkunActivity.class);
+        startActivity(intent);
+    }
+    public void list(View view) {
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intent);
+    }
+    public void belanja(View view) {
+        Intent intent = new Intent(MainActivity.this, BelanjaActivity.class);
+        startActivity(intent);
+    }
+
+
 }
